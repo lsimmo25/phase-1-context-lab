11a6bc23b2705a278d532ff31a254f36e0809e28
Lyle Simmons <lylesimmons125@gmail.com>
2024-02-16 15:32:52 -0500

    work in progress

--- index.js
+++ index.js
@@ -8,16 +8,60 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ const createTimeInEvent = function(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date
+    });
+};
+
+const createTimeOutEvent = function(dateStamp) {
+    const [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date
+    });
+};
+
+class EmployeeRecord {
+    constructor(firstName, familyName, title, payPerHour, timeInEvents = [], timeOutEvents = []) {
+        this.firstName = firstName;
+        this.familyName = familyName;
+        this.title = title;
+        this.payPerHour = payPerHour;
+        this.timeInEvents = timeInEvents;
+        this.timeOutEvents = timeOutEvents;
+        this.createTimeInEvent = createTimeInEvent.bind(this);
+        this.createTimeOutEvent = createTimeOutEvent.bind(this);
+    }
+}
+
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return new EmployeeRecord(
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      [],
+      []
+    );
+}
+
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(createEmployeeRecord);
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+        return e.date;
+    });
 
     const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+        return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this), 0);
 
-    return payable
-}
-
+    return payable;
+};
